4f1fce3bfecd41b7431ef47aa26a6740c0a2500b
John Fitzpatrick <jfitz@computer.org>
2013-07-29 17:18:43 -0400

    Added stem function

--- data_analysis.py
+++ data_analysis.py
@@ -10,6 +10,31 @@ import numpy
 
 jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
+def condense(points):
+	points.sort()
+	result = {
+		'sum': sum(points),
+		'count':len(points),
+		'minimum': min(points),
+		'maximum': max(points),
+		'median': numpy.median(points),
+		'upper_quartile': points[math.trunc(round(len(points)*0.75))],
+		'lower_quartile': points[math.trunc(round(len(points)*0.25))]
+	}
+	return result
+
+def stem_split(values, num_branches, scale_factor):
+	stem = []
+	for i in range(num_branches):
+		minimum = i * scale_factor
+		maximum = (i + 1) * scale_factor
+		branch_values = []
+		for value in values:
+			if value >= minimum and value < maximum:
+				branch_values.append(value)
+		stem.append( { 'minimum': minimum, 'maximum': maximum, 'values': branch_values } )
+	return stem
+
 class MainPage(webapp.RequestHandler):
 	def get(self):
 		template_values = { }
@@ -18,26 +43,25 @@ class MainPage(webapp.RequestHandler):
 
 class Condense(webapp.RequestHandler):
 	def post(self):
-		point_data = json.loads(self.request.body)
-		point_data.sort()
-		t_list = {
-			'sum': sum(point_data),
-			'count':len(point_data),
-			'minimum': min(point_data),
-			'maximum': max(point_data),
-			'median': numpy.median(point_data),
-			'upper_quartile': point_data[math.trunc(round(len(point_data)*0.75))],
-			'lower_quartile': point_data[math.trunc(round(len(point_data)*0.25))]
-		}
-		s = json.dumps( t_list )
-		
 		self.response.headers['Content-Type'] = 'application/json'
-		self.response.out.write(s)
-
+		self.response.out.write( json.dumps( condense( json.loads(self.request.body) ) ) )
+		
+class Stem(webapp.RequestHandler):
+	def post(self):
+		self.response.headers['Content-Type'] = 'application/json'
+		points = json.loads(self.request.body)
+		condensed_points = condense( points )
+		scale_factor = 5
+		interquartile_distance = condensed_points['maximum'] - condensed_points['minimum']
+		num_branches = math.trunc( round((interquartile_distance / scale_factor) + 0.5) ) + 1
+		stem = stem_split(points, num_branches, scale_factor)
+		self.response.out.write( json.dumps( stem ) )
+		
 application = webapp.WSGIApplication(
 	[
 		('/', MainPage),
-		('/condense', Condense)
+		('/condense', Condense),
+		('/stem', Stem)
 	],
 	debug=False)
 
