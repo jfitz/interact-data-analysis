484235f0759d0c5286c70f751974f105bd91df5d
John Fitzpatrick <jfitz@computer.org>
2013-08-14 19:04:25 -0400

    create step size

--- data_analysis.py
+++ data_analysis.py
@@ -11,18 +11,14 @@ import numpy
 
 jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
-def power_to_factor(power):
-	return pow(10.0, power)
-
 def front(value, power):
-	factor = power_to_factor(power)
-	return math.trunc((value / factor) + 0.5)
+	return math.trunc((value / pow(10.0, power)) + 0.5)
 
 def fraction(value):
 	return value - math.trunc(value)
 
 def remainder(value, power):
-	factor = power_to_factor(power)
+	factor = pow(10.0, power)
 	format = '%0' + str(power) + 'd'
 	x1 = fraction(value / factor) * factor
 	x2 = format % x1
@@ -30,9 +26,9 @@ def remainder(value, power):
 
 def compute_power(maximum_value):
 	power = 1
-	while (maximum_value / power_to_factor(power)) > 20:
+	while (maximum_value / pow(10.0, power)) > 20:
 		power += 1
-	return power
+	return (pow(10.0, power), power)
 
 def condense(points):
 	points.sort()
@@ -47,18 +43,19 @@ def condense(points):
 	}
 	return result
 
-def stem_split(values, num_branches, power):
+def stem_split(values, num_branches, step_size, power):
+	factor = pow(10.0, power)
+	round_delta = (0.05 * factor)
 	branches = []
-	factor = power_to_factor(power)
 	for i in range(num_branches):
-		minimum = i * factor
-		maximum = (i + 1) * factor
+		range_minimum = i * factor
+		range_maximum = range_minimum + step_size
 		branch_values = []
 		for value in values:
-			rounded_value = value + (0.05 * factor)
-			if rounded_value >= minimum and rounded_value < maximum:
+			rounded_value = value + round_delta
+			if rounded_value >= range_minimum and rounded_value < range_maximum:
 				branch_values.append(rounded_value)
-		branches.append( { 'minimum': minimum, 'maximum': maximum, 'values': branch_values } )
+		branches.append( { 'minimum': range_minimum, 'maximum': range_maximum, 'values': branch_values } )
 	return branches
 
 def stem_graph(branches, power):
@@ -96,23 +93,23 @@ class Stem(webapp.RequestHandler):
 		self.response.headers['Content-Type'] = 'application/json'
 		points = json.loads(self.request.body)
 		condensed_points = condense( points )
-		range = condensed_points["maximum"] - condensed_points["minimum"]
-		power = compute_power(range)
-		interquartile_distance = condensed_points['maximum'] - condensed_points['minimum']
-		scale_factor = power_to_factor(power)
-		num_branches = math.trunc( round((interquartile_distance / scale_factor) + 0.5) ) + 1
-		stem = stem_split(points, num_branches, power)
+		data_range = condensed_points["maximum"] - condensed_points["minimum"]
+		(step_size, power) = compute_power(data_range)
+		num_branches = math.trunc( round((data_range / pow(10.0, power)) + 0.5) ) + 1
+		stem = stem_split(points, num_branches, step_size, power)
 		self.response.out.write( json.dumps( stem ) )
 		
 class StemGraph(webapp.RequestHandler):
 	def post(self):
 		body = self.request.body
 		stem = json.loads(body)
-		maximum = 0
+		range_maximum = 0
+		range_minimum = 0
 		for branch in stem:
-			maximum = max(branch["maximum"], maximum)
-		power = compute_power(maximum)
-		leader_length = len(str(math.trunc(maximum))) - power
+			range_maximum = max(branch["maximum"], range_maximum)
+			range_minimum = min(branch["minimum"], range_minimum)
+		(step_size, power) = compute_power(range_maximum)
+		leader_length = len(str(math.trunc(range_maximum))) - power
 		leader_format = '%0' + str(leader_length) + 'd'
 		stem_tuples = stem_graph(stem, power)
 		stem_texts = stem_tuples_to_text(stem_tuples, leader_format)
